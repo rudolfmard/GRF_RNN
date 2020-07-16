153a3dde44bed8c318c2c00eb75cb24a61a9e9d9
rudolfmard <rudolf.mard@gmail.com>
2020-07-16 22:11:10 +0300

    tried shaping data for the rnn, trying different optimizers and learning rates

--- grf_rnn.py
+++ grf_rnn.py
@@ -19,7 +19,7 @@ grf3 = []
 grf4 = []
 
 #no. of the trial at index 41
-ANGLE_INDEX = (14, 15, 16, 17, 18)
+ANGLE_INDEX = (13, 14, 15, 16, 17)
 GRF_INDEX = (42, 43, 44)
 for row in data:
     if row[41] == 1:
@@ -78,8 +78,44 @@ for row in data:
 print("A1: ", angles1.shape, ", A2: ", angles2.shape, ", A3: ", angles3.shape, ", A4: ", angles4.shape)
 print("G1: ", grf1.shape, ", G2: ", grf2.shape, ", G3: ", grf3.shape, ", G4: ", grf4.shape)
 
-if False:
-#construct the model
+#stack three angle sets for training
+angles = np.vstack((angles1, angles2))
+angles = np.vstack((angles, angles3))
+print("angle shape: ", angles.shape)
+train = np.reshape(angles, (1,  angles.shape[0], angles.shape[1]))
+print("reshaped: ", train.shape)
 
-    model = Sequential(SimpleRNN(3, input_shape=(5,1)))
+#stack three grf sets for training
+grf = np.vstack((grf1, grf2))
+grf = np.vstack((grf, grf3))
+print("grf shape: ", grf.shape)
+'''goal = np.reshape(grf, (grf.shape[0], grf.shape[1], 1))'''
+goal = np.reshape(grf, (1, grf.shape[0], grf.shape[1]))
+print("reshaped: ", goal.shape)
+
+#one set of angles and grf for testing
+test_angles = np.reshape(angles4, (1, angles4.shape[0], angles4.shape[1]))
+test_grf = np.reshape(grf4, (1, grf4.shape[0], grf4.shape[1]))
+
+
+if True:
+#construct the model
+    EPOCHS = 100
+    BATCH_SIZE = 10
+
+    model = Sequential(SimpleRNN(3, input_shape=(None,5), return_sequences=True))
+    model.add(Dense(3))
+
+    #different optmimizers
+    adam = keras.optimizers.Adam(learning_rate=0.5)
+    rmsprop = keras.optimizers.RMSprop(learning_rate=0.5)
+    adadelta = keras.optimizers.Adadelta(learning_rate=0.5)
+
+    model.compile(optimizer=rmsprop, loss="mse")
     model.summary()
+    print("fitting the model...")
+    model.fit(train, goal, epochs= EPOCHS, batch_size= BATCH_SIZE)
+
+    print("evaluating...")
+    results = model.evaluate(test_angles, test_grf, batch_size= BATCH_SIZE)
+
